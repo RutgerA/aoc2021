d536efbef06fe59b44f1a17555ef991863c3bf44
Rutger Alders <rutger.alders@cegeka.com>
2021-12-23 09:08:38 +0100

    Added remaining days

---
+++ day25/solution1.js
@@ -0,0 +1,23 @@
+"use strict"
+
+const assert = require('assert');
+
+const { data } = require('./data');
+const { example } = require('./example');
+
+
+function temp(values) {
+	
+}
+
+function run() {
+    const exampleResult = temp(example);
+    assert.equal(exampleResult, 195);
+
+    const result = temp(data);
+    assert.equal(result, 237);
+
+    console.log('Answer:', result);
+}
+
+run();
